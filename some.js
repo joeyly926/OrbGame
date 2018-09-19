8552deb8bbadbf79f04b83f3b8ddcb1cdd0fec09
joeyly926 <joeyly926@gmail.com>
2018-09-19 12:03:43 -0400

    Delete some.js

--- some.js
+++
@@ -1,9 +0,0 @@
-var pics = [ 'white.png', 'milk.png', 'dark.png'];
-
-window.setInterval( "changepic()", 2000 )
-
-function changepic(){
-	chocolate = document.getElementById("chocolate");
-	randomChocolate = pics[Math.floor(Math.random()*pics.length)];
-	chocolate.setAttribute("src", randomchocolate);
-}
